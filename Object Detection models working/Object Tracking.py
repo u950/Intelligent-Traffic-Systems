46f4c592e469f769df23917ae415857786b12cd3
UDAY BIDDIKA <103553174+u950@users.noreply.github.com>
2025-02-08 15:51:12 +0530

    Create Object Tracking.py

---
+++ Object Detection models working/Object Tracking.py
@@ -0,0 +1,286 @@
+#!/usr/bin/env python3
+
+################################################################################
+# SPDX-FileCopyrightText: Copyright (c) 2019-2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+
+import sys
+sys.path.append('../')
+import platform
+import configparser
+
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import GLib, Gst
+from common.platform_info import PlatformInfo
+from common.bus_call import bus_call
+
+import pyds
+
+PGIE_CLASS_ID_VEHICLE = 0
+PGIE_CLASS_ID_BICYCLE = 1
+PGIE_CLASS_ID_PERSON = 2
+PGIE_CLASS_ID_ROADSIGN = 3
+MUXER_BATCH_TIMEOUT_USEC = 33000
+
+def osd_sink_pad_buffer_probe(pad,info,u_data):
+    frame_number=0
+    obj_counter = {
+        PGIE_CLASS_ID_VEHICLE:0,
+        PGIE_CLASS_ID_PERSON:0,
+        PGIE_CLASS_ID_BICYCLE:0,
+        PGIE_CLASS_ID_ROADSIGN:0
+    }
+    num_rects=0
+    gst_buffer = info.get_buffer()
+    if not gst_buffer:
+        print("Unable to get GstBuffer ")
+        return
+
+    batch_meta = pyds.gst_buffer_get_nvds_batch_meta(hash(gst_buffer))
+    l_frame = batch_meta.frame_meta_list
+    while l_frame is not None:
+        try:
+            frame_meta = pyds.NvDsFrameMeta.cast(l_frame.data)
+        except StopIteration:
+            break
+
+        frame_number=frame_meta.frame_num
+        l_obj=frame_meta.obj_meta_list
+        while l_obj is not None:
+            try:
+                obj_meta=pyds.NvDsObjectMeta.cast(l_obj.data)
+            except StopIteration:
+                break
+            
+            obj_counter[obj_meta.class_id] += 1
+            
+            # Display class name for each object
+            display_meta = pyds.nvds_acquire_display_meta_from_pool(batch_meta)
+            display_meta.num_labels = 1
+            py_nvosd_text_params = display_meta.text_params[0]
+            
+            # Get class name from object metadata
+            py_nvosd_text_params.display_text = pyds.get_string(obj_meta.obj_label)
+            
+            # Position the text above the object
+            py_nvosd_text_params.x_offset = int(obj_meta.rect_params.left)
+            py_nvosd_text_params.y_offset = int(obj_meta.rect_params.top - 10)
+            
+            # Set font properties
+            py_nvosd_text_params.font_params.font_name = "Serif"
+            py_nvosd_text_params.font_params.font_size = 11
+            py_nvosd_text_params.font_params.font_color.set(1.0, 1.0, 1.0, 1.0)
+            
+            # Set background color
+            py_nvosd_text_params.set_bg_clr = 1
+            py_nvosd_text_params.text_bg_clr.set(0.0, 0.0, 0.0, 1.0)
+            
+            pyds.nvds_add_display_meta_to_frame(frame_meta, display_meta)
+            
+            try:
+                l_obj=l_obj.next
+            except StopIteration:
+                break
+        
+        try:
+            l_frame=l_frame.next
+        except StopIteration:
+            break
+            
+    return Gst.PadProbeReturn.OK
+
+def main(args):
+    # Check input arguments
+    if(len(args)<2):
+        sys.stderr.write("usage: %s <h264_elementary_stream>\n" % args[0])
+        sys.exit(1)
+
+    platform_info = PlatformInfo()
+    # Standard GStreamer initialization
+
+    Gst.init(None)
+
+    # Create gstreamer elements
+    # Create Pipeline element that will form a connection of other elements
+    print("Creating Pipeline \n ")
+    pipeline = Gst.Pipeline()
+
+    if not pipeline:
+        sys.stderr.write(" Unable to create Pipeline \n")
+
+    # Source element for reading from the file
+    print("Creating Source \n ")
+    source = Gst.ElementFactory.make("filesrc", "file-source")
+    if not source:
+        sys.stderr.write(" Unable to create Source \n")
+
+    # Since the data format in the input file is elementary h264 stream,
+    # we need a h264parser
+    print("Creating H264Parser \n")
+    h264parser = Gst.ElementFactory.make("h264parse", "h264-parser")
+    if not h264parser:
+        sys.stderr.write(" Unable to create h264 parser \n")
+
+    # Use nvdec_h264 for hardware accelerated decode on GPU
+    print("Creating Decoder \n")
+    decoder = Gst.ElementFactory.make("nvv4l2decoder", "nvv4l2-decoder")
+    if not decoder:
+        sys.stderr.write(" Unable to create Nvv4l2 Decoder \n")
+
+    # Create nvstreammux instance to form batches from one or more sources.
+    streammux = Gst.ElementFactory.make("nvstreammux", "Stream-muxer")
+    if not streammux:
+        sys.stderr.write(" Unable to create NvStreamMux \n")
+
+    # Use nvinfer to run inferencing on decoder's output,
+    # behaviour of inferencing is set through config file
+    pgie = Gst.ElementFactory.make("nvinfer", "primary-inference")
+    if not pgie:
+        sys.stderr.write(" Unable to create pgie \n")
+
+    tracker = Gst.ElementFactory.make("nvtracker", "tracker")
+    if not tracker:
+        sys.stderr.write(" Unable to create tracker \n")
+
+    sgie2 = Gst.ElementFactory.make("nvinfer", "secondary2-nvinference-engine")
+    if not sgie2:
+        sys.stderr.write(" Unable to make sgie2 \n")
+
+    nvvidconv = Gst.ElementFactory.make("nvvideoconvert", "convertor")
+    if not nvvidconv:
+        sys.stderr.write(" Unable to create nvvidconv \n")
+
+    # Create OSD to draw on the converted RGBA buffer
+    nvosd = Gst.ElementFactory.make("nvdsosd", "onscreendisplay")
+
+    if not nvosd:
+        sys.stderr.write(" Unable to create nvosd \n")
+
+    # Finally render the osd output
+    if platform_info.is_integrated_gpu():
+        print("Creating nv3dsink \n")
+        sink = Gst.ElementFactory.make("nv3dsink", "nv3d-sink")
+        if not sink:
+            sys.stderr.write(" Unable to create nv3dsink \n")
+    else:
+        if platform_info.is_platform_aarch64():
+            print("Creating nv3dsink \n")
+            sink = Gst.ElementFactory.make("nv3dsink", "nv3d-sink")
+        else:
+            print("Creating EGLSink \n")
+            sink = Gst.ElementFactory.make("nveglglessink", "nvvideo-renderer")
+        if not sink:
+            sys.stderr.write(" Unable to create egl sink \n")
+
+    print("Playing file %s " %args[1])
+    source.set_property('location', args[1])
+    streammux.set_property('width', 1920)
+    streammux.set_property('height', 1080)
+    streammux.set_property('batch-size', 1)
+    streammux.set_property('batched-push-timeout', MUXER_BATCH_TIMEOUT_USEC)
+
+    #Set properties of pgie and sgie
+    pgie.set_property('config-file-path', "dstest2_pgie_config.txt")
+    sgie2.set_property('config-file-path', "dstest2_sgie2_config.txt")
+
+    #Set properties of tracker
+    config = configparser.ConfigParser()
+    config.read('dstest2_tracker_config.txt')
+    config.sections()
+
+    for key in config['tracker']:
+        if key == 'tracker-width' :
+            tracker_width = config.getint('tracker', key)
+            tracker.set_property('tracker-width', tracker_width)
+        if key == 'tracker-height' :
+            tracker_height = config.getint('tracker', key)
+            tracker.set_property('tracker-height', tracker_height)
+        if key == 'gpu-id' :
+            tracker_gpu_id = config.getint('tracker', key)
+            tracker.set_property('gpu_id', tracker_gpu_id)
+        if key == 'll-lib-file' :
+            tracker_ll_lib_file = config.get('tracker', key)
+            tracker.set_property('ll-lib-file', tracker_ll_lib_file)
+        if key == 'll-config-file' :
+            tracker_ll_config_file = config.get('tracker', key)
+            tracker.set_property('ll-config-file', tracker_ll_config_file)
+
+    print("Adding elements to Pipeline \n")
+    pipeline.add(source)
+    pipeline.add(h264parser)
+    pipeline.add(decoder)
+    pipeline.add(streammux)
+    pipeline.add(pgie)
+    pipeline.add(tracker)
+    pipeline.add(sgie2)
+    pipeline.add(nvvidconv)
+    pipeline.add(nvosd)
+    pipeline.add(sink)
+
+    # we link the elements together
+    # file-source -> h264-parser -> nvh264-decoder ->
+    # nvinfer -> nvvidconv -> nvosd -> video-renderer
+    print("Linking elements in the Pipeline \n")
+    source.link(h264parser)
+    h264parser.link(decoder)
+
+    sinkpad = streammux.request_pad_simple("sink_0")
+    if not sinkpad:
+        sys.stderr.write(" Unable to get the sink pad of streammux \n")
+    srcpad = decoder.get_static_pad("src")
+    if not srcpad:
+        sys.stderr.write(" Unable to get source pad of decoder \n")
+    srcpad.link(sinkpad)
+    streammux.link(pgie)
+    pgie.link(tracker)
+    tracker.link(sgie2)
+    sgie2.link(nvvidconv)
+    nvvidconv.link(nvosd)
+    nvosd.link(sink)
+
+
+    # create and event loop and feed gstreamer bus mesages to it
+    loop = GLib.MainLoop()
+
+    bus = pipeline.get_bus()
+    bus.add_signal_watch()
+    bus.connect ("message", bus_call, loop)
+
+    # Lets add probe to get informed of the meta data generated, we add probe to
+    # the sink pad of the osd element, since by that time, the buffer would have
+    # had got all the metadata.
+    osdsinkpad = nvosd.get_static_pad("sink")
+    if not osdsinkpad:
+        sys.stderr.write(" Unable to get sink pad of nvosd \n")
+    osdsinkpad.add_probe(Gst.PadProbeType.BUFFER, osd_sink_pad_buffer_probe, 0)
+
+
+    print("Starting pipeline \n")
+    
+    # start play back and listed to events
+    pipeline.set_state(Gst.State.PLAYING)
+    try:
+      loop.run()
+    except:
+      pass
+
+    # cleanup
+    pipeline.set_state(Gst.State.NULL)
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
+
